2b86a17dc3a822e961284535a6a7655eeba7fc4a
Tim <tim@tim-ings.com>
2019-06-18 19:53:43 +0800

    Adds curse button and tech tags

--- portfolio/src/views/Portfolio.js
+++ portfolio/src/views/Portfolio.js
@@ -7,6 +7,14 @@ import Button from 'react-bootstrap/Button';
 import CardColumns from 'react-bootstrap/CardColumns';
 import Container from 'react-bootstrap/Container';
 
+class TechTag extends React.Component {
+    render() {
+        return (
+            <span className="tech-tag">{this.props.tech}</span>
+        );
+    }
+}
+
 class Project extends React.Component {
 
     constructor(props) {
@@ -15,10 +23,11 @@ class Project extends React.Component {
     }
 
     render() {
-        // send the user to the github releases unless a live demo exists
+        // default to github releases
         let demo_url = this.baseGitHubUrl + this.props.data.slug + "/releases";
         let demo_label = "Download";
         let demo_icon = (<img style={{height: '1rem', transform: 'translateY(-2px)'}} src="img/logos/download_light.png" alt="" />);
+        // check if a live demo is available
         if (this.props.data.live_demo_url) {
             demo_url = this.props.data.live_demo_url;
             demo_label = "Live Demo";
@@ -32,13 +41,42 @@ class Project extends React.Component {
                     <circle cx="50" cy="50" r="50" style={{fill: 'red'}}/>
                 </svg>);
         }
+        // check if this project is on curse
         if (this.props.data.curse_url) {
-
+            demo_url = this.props.data.curse_url;
+            demo_label = "View on Curse";
+            demo_icon = (<img style={{height: '1rem', transform: 'translateY(-2px)'}} src="img/logos/curse.png" alt="" />);
         }
 
         let header_img_url = "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzEwNC84MTkvb3JpZ2luYWwvY3V0ZS1raXR0ZW4uanBn"
         if (this.props.data.header_img_url) {
             header_img_url = this.props.data.header_img_url;
+        }
+
+        let btn_github = (
+            <Button target="_blank" rel="noopener noreferrer" style={{margin: '5px'}} href={this.baseGitHubUrl + this.props.data.slug}>
+                <img style={{
+                    height: '1rem',
+                    transform: 'translateY(-2px)'
+                }} src="img/logos/github_light.png"></img>
+                <span style={{marginLeft: '5px'}}>GitHub</span>
+            </Button>
+        );
+
+        let btn_demo = (
+            <Button target="_blank" rel="noopener noreferrer" style={{margin: '5px'}} href={demo_url}>
+                {demo_icon}
+                <span style={{marginLeft: '5px'}}>{demo_label}</span>
+            </Button>
+        );
+        if (this.props.data.hide_download) {
+            btn_demo = null;
+        }
+
+        let tech_tags = [];
+        for (let i = 0; i < this.props.data.tech.length; i++) {
+            let tt = this.props.data.tech[i];
+            tech_tags.push(<TechTag tech={tt} />);
         }
 
         return (
@@ -47,18 +85,12 @@ class Project extends React.Component {
                 <Card.Body>
                     <Card.Title>{this.props.data.name}</Card.Title>
                     <Card.Text>{this.props.data.description}</Card.Text>
-                    <Button target="_blank" rel="noopener noreferrer" style={{margin: '5px'}} href={this.baseGitHubUrl + this.props.data.slug}>
-                        <img style={{
-                            height: '1rem',
-                            transform: 'translateY(-2px)'
-                        }} src="img/logos/github_light.png"></img>
-                        <span style={{marginLeft: '5px'}}>GitHub</span>
-                    </Button>
-                    <Button target="_blank" rel="noopener noreferrer" style={{margin: '5px'}} href={demo_url}>
-                        {demo_icon}
-                        <span style={{marginLeft: '5px'}}>{demo_label}</span>
-                    </Button>
+                    {tech_tags}
+                    <br />
+                    {btn_github}
+                    {btn_demo}
                 </Card.Body>
+                
             </Card>
         )
     }
